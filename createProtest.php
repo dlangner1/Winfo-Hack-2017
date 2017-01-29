ad3e4ac68ea9d32195bee179b3149ba6a4e9fa4a
Dustin Langner <dustin.langner1@gmail.com>
2017-01-28 17:15:44 -0800

    finish stuff:

--- createProtest.php
+++ createProtest.php
@@ -1,8 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Karen
- * Date: 1/28/2017
- * Time: 11:52 AM
- */
+    $name = $_POST["name"];
+    $city = $_POST["city"];
+    $state = $_POST["state"];
+    $date = $_POST["date"];
+    $start_time = $_POST["time"];
+    $purpose = $_POST["purpose"];
+    $long_description = $_POST["long"];
+
+    // redirects the user to a particular page and then kills the page
+    function redirect_page($location) {
+        header("Location: $location");
+        die();
+    }
+
+    file_put_contents("events.txt", "$name;$city;$state;$date;$start_time;$purpose;$long_description", FILE_APPEND);
+    redirect_page("protests.php");
+
+
+
+
+
 
